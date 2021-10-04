a70400b9df50710c27f6efbee3efcba0b1aea748
Akash Mahmud <mahmudakash177@gmail.com>
2021-10-04 20:02:17 +0600

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,7 @@ app.get("/api/config/google", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use("/logo", express.static(path.join(__dirname, "/logo")));
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
